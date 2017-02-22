7ce955f47cf05f2fb601905ead8bbb8d71048d85
EricRiney <fails.to.install@gmail.com>
2017-02-22 10:50:20 -0800

    blah

--- build/client.js
+++ build/client.js
@@ -1,6 +1,10 @@
 'use strict';
 var ip = require("ip");
-var net = require('net'), readline = require('readline'), client = new net.Socket(), io = readline.createInterface(process.stdin, process.stdout);
+var net = require('net'), 
+readline = require('readline'), 
+client = new net.Socket(), 
+io = readline.createInterface(process.stdin, process.stdout);
+
 client.on('data', function (data) {
 });
 client.on('close', function () {
